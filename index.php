40b0d72ed707d4096d0a210ba909cac2af70932d
IBIALO <IBIALO@F60735.ad.demathieu-bard.fr>
2018-05-11 10:11:14 +0200

    Correction index

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ dbproject\conf\ConnexionBase::initialisation('src/conf/conf.ini');
 //Initialisation de Slim
 $app = new \Slim\Slim(array(
     'cookies.encrypt' => true,
-    'cookies.secret_key' => 'dBPorjecT',
+    'cookies.secret_key' => 'dBProjecT',
     'cookies.cipher' => MCRYPT_RIJNDAEL_256,
     'cookies.cipher_mode' => MCRYPT_MODE_CBC
 ));
